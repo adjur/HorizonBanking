9a014794014093c85c63a2c6b7ce0d0a0fa88cd5
Andi Dj <155870242+adjur@users.noreply.github.com>
2024-06-03 10:41:04 -0700

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    eslint: {
+        isnoreDuringBuilds: true
+    }
+};
 
 export default nextConfig;
